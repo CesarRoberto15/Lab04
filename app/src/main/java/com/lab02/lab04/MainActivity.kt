6271a113058742c4fac52d0419b18b5907e78512
Cesar Ancco <canccor@hotmail.com>
2022-05-20 18:36:04 -0500

    App v2.0

--- app/src/main/java/com/lab02/lab04/MainActivity.kt
+++ app/src/main/java/com/lab02/lab04/MainActivity.kt
@@ -2,40 +2,79 @@ package com.lab02.lab04
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.widget.*
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collect
 
 class MainActivity : AppCompatActivity() {
 
     private val Context.dataStore by preferencesDataStore(NotePrefs.PREFS_NAME)
     lateinit var notePrefs: NotePrefs
-    lateinit var floatingActionButton: FloatingActionButton
+    lateinit var btnBlue:Button
+    lateinit var btnRed:Button
+
+    lateinit var btnSerif:Button
+    lateinit var btnMonospace:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lateinit var layoutConst: LinearLayout
-        notePrefs = NotePrefs(dataStore)
+        btnBlue = findViewById(R.id.blue)
+        btnRed = findViewById(R.id.red)
+
+        btnSerif=findViewById(R.id.serif)
+        btnMonospace=findViewById(R.id.monospace)
+
+        lateinit var layoutConst : LinearLayout
+
+        lateinit var txtPeriodo : TextView
+        lateinit var edtPeriodo : EditText
+
+        notePrefs=NotePrefs(dataStore)
 
         layoutConst = findViewById(R.id.LayoutConst)
-        layoutConst.setBackgroundColor(Color.RED)
 
-        floatingActionButton = findViewById(R.id.floatingActionButton)
+        txtPeriodo=findViewById(R.id.periodo)
+        edtPeriodo=findViewById(R.id.periodoEdit)
 
-        floatingActionButton.setOnClickListener {
-            lifecycleScope.launch {
-                notePrefs.backgroundColor.collect { mycolor ->
-                    layoutConst.setBackgroundColor(Integer.parseInt(mycolor.toString()))
-                }
+        //Recogiendo el color
+        lifecycleScope.launch {
+            notePrefs.backgroundColor.collect { mycolor ->
+                layoutConst.setBackgroundColor(Integer.parseInt(mycolor))
+
             }
         }
 
+        //Guardando el color por el respectivo boton
+        btnBlue.setOnClickListener{
+            lifecycleScope.launch{
+                notePrefs.saveNoteBackgroundColor(Color.BLUE.toString())
+            }
+        }
+
+        btnRed.setOnClickListener{
+            lifecycleScope.launch{
+                notePrefs.saveNoteBackgroundColor(Color.RED.toString())
+            }
+        }
+
+        btnSerif.setOnClickListener{
+            lifecycleScope.launch{
+                notePrefs.saveNoteTypeface(Typeface.SERIF.toString())
+            }
+        }
+        btnMonospace.setOnClickListener{
+            lifecycleScope.launch{
+                notePrefs.saveNoteTypeface(Typeface.MONOSPACE.toString())
+            }
+        }
 
     }
 
--- app/src/main/java/com/lab02/lab04/NotePrefs.kt
+++ app/src/main/java/com/lab02/lab04/NotePrefs.kt
@@ -1,6 +1,8 @@
 package com.lab02.lab04
 
 import android.graphics.Color
+import android.graphics.Typeface
+import android.webkit.WebSettings
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
@@ -11,9 +13,19 @@ import kotlinx.coroutines.flow.map
 class NotePrefs(
     private val dataStore: DataStore<Preferences>
 ) {
-    suspend fun saveNoteBackgroundColor(noteBackgroundColor: String) {
+    suspend fun saveNoteBackgroundColor(color : String) {
         dataStore.edit { preferences ->
-            preferences[BACKGROUND_COLOR] = noteBackgroundColor
+            preferences[BACKGROUND_COLOR] = color
+        }
+    }
+    suspend fun saveNoteTypeface(type:String){
+        dataStore.edit { preferences ->
+            preferences[TYPE_FACE]=type
+        }
+    }
+    suspend fun saveNoteTextSize(size:String){
+        dataStore.edit { preferences ->
+            preferences[TEXT_SIZE]=size
         }
     }
 
@@ -22,9 +34,22 @@ class NotePrefs(
             preferences[BACKGROUND_COLOR] ?: Color.CYAN.toString()
         }
 
+    val typeface: Flow<String>
+        get()=dataStore.data.map { preferences ->
+            preferences[TYPE_FACE] ?: Typeface.DEFAULT.toString()
+        }
+
+    val textSize: Flow<String>
+        get()=dataStore.data.map { preferences ->
+            preferences[TEXT_SIZE] ?: WebSettings.TextSize.NORMAL.toString()
+        }
+
+
     companion object {
         val PREFS_NAME = "PREFS_NAME"
         private val BACKGROUND_COLOR = stringPreferencesKey("key_app_background_color")
+        private val TYPE_FACE = stringPreferencesKey("key_app_type_face")
+        private val TEXT_SIZE = stringPreferencesKey("key_app_text_size")
 
     }
 }
